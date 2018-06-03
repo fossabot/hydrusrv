5c6858d02405bcaf6b10d411e769ff98a392d3af
Michael Serajnik <ms@mserajnik.at>
2018-06-03 22:17:14 +0200

    Update changelog and version numbers for release 2.1.0

--- server/routes/index.js
+++ server/routes/index.js
@@ -7,7 +7,7 @@ module.exports = app => {
   app.get(`${config.apiBase}`, (req, res, next) => {
     res.send({
       hydrusrv: {
-        version: '2.0.1'
+        version: '2.1.0'
       }
     })
   })
