431d4ae88a0287282714f70183fb8309c7586937
Michael Serajnik <ms@mserajnik.at>
2018-08-22 20:34:06 +0200

    Update changelog and version numbers for release 3.6.0

--- server/routes/index.js
+++ server/routes/index.js
@@ -7,7 +7,7 @@ module.exports = app => {
   app.get(`${config.apiBase}`, (req, res, next) => {
     res.send({
       hydrusrv: {
-        version: '3.5.1'
+        version: '3.6.0'
       }
     })
   })
